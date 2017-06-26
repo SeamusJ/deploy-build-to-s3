f634b44e64f51541cc529e23e905d5d5457b678b
Seamus Johnson <seamus.johnson@censis.com>
2017-06-26 16:28:45 -0500

    Refactored gulp task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@ let gulp = require("gulp");
 let zip = require("gulp-zip");
 
 let sourceFiles = [
-    "./*.js",
-    "node_modules/**/*.*"
+    "./**/*",
+    "!./*.ts",
+    "!./*.json",
+    "!./*.zip",
+    "!./gulpfile.js",
+    "!./.gitignore"
 ];
 
 gulp.task("build:zip", () => {
-    gulp.src(sourceFiles)
+    gulp.src(sourceFiles, { dot: true, base: "./" })
         .pipe(zip("deploy-build-to-s3.zip"))
         .pipe(gulp.dest("./"));
 });
